b6e926afec2e87ae033bcf7ded9772185017a154
nicklouko <loukopoulosn@gmail.com>
2025-08-24 00:39:15 +0300

    navigationbar
    worked on top navigation bar

---
+++ navigationbar.php
@@ -0,0 +1,14 @@
+<html>
+  <head><link rel="stylesheet" href="style.css"></head> 
+  <body>
+<nav class="navbar">
+  <ul class="navpages">
+    <li id="homelink"><a  href="index.php">Home</a></li>
+    <li><a href="mydishes.php">My Dishes</a></li>
+    <li><a href="viewdishes.php">View Dishes</a></li>
+    <li id="login"><a  href="login.php">Log In</a></li>
+  </ul>
+
+</nav>
+</body>
+</html> 
