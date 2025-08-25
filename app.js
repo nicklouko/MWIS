219c0176511e6217ca63443a93206a27061b787d
nicklouko <loukopoulosn@gmail.com>
2025-08-25 21:36:24 +0300

    finished /register route
    changed shadow color on auth forms, finished registration route

--- app.js
+++ app.js
@@ -21,9 +21,32 @@ const db = mysql.createPool({
 
 
 // Registration route
-app.post('/register', (req, res) => {
-    const{ email, username, password} =req.body;
-    db.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', [email, username, password])
+app.post('/register', async (req, res) => {
+  try{
+    const { email, username, password } = req.body;
+
+    //check if email already exists
+    const [rowsEmail] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
+    if (rowsEmail.length > 0) {
+      return res.status(400).send({ message: 'Email already in use' });
+    }
+    // Check if user already exists
+    const [rows] = await db.execute('SELECT * FROM users WHERE username = ?', [username]);
+    if (rows.length > 0) { 
+      return res.status(400).send({ message: 'User already exists' });
+    }
+    // Insert new user
+    await db.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', [email, username, password]);
+    return res.status(201).json({
+      message: 'User registered successfully',
+      user: { email, username }
+    });
+  } catch (err) {
+    console.error('Error registering user:', err);
+    return res.status(500).send({ message: 'Error registering user' });
+  }       });
+
+    /*   db.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', [email, username, password])
       .then(() => console.log('User registered:', email))
       .catch(err => {
         console.error('Error registering user:', err);
@@ -31,7 +54,10 @@ app.post('/register', (req, res) => {
       });
 
     res.status(201).send({ message: 'User registered successfully', user: { email, username } });
-});
+});*/
+
+    
+ 
 
 // Login route
 app.post('/login', (req, res) => {
